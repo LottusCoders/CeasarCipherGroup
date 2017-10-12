2914a695702a78d37159a19955116cbb06643e36
DDCenteno <danielacentenoramos@gmail.com>
2017-10-12 09:15:16 -0500

    habersh

--- Javascript/app2.js
+++ Javascript/app2.js
@@ -13,40 +13,40 @@ var option=prompt("Elija una opciones "+"\n" + "1. Cifrar "+"\n" + "2. Decifrar"
       
                                     //FUNCION PARA CIFRAR
 function cipher(phrase){
-  var StrLet ="";                                  //Creamos el string vacio donde se almacenara losnuevos valores.
+  var strLet ="";                                  //Creamos el string vacio donde se almacenara losnuevos valores.
  
   for(i=0;i<phrase.length;i++){                    //Recorremosla frase ingresada,Si esta en el rango de MAYUSCULA.
       var Asc = phrase.charCodeAt(i)               //Obtenemos el codigo ASCII de cada letra
       if(Asc>=65 && Asc<=90){                      //Rango para solo las mayusculas
          var NewAsc = (Asc-65 +33)%26 + 65;        // Aplicamos la formula para obtener la nueva position ASCII.
          var Newlet= String.fromCharCode(NewAsc);  //Desciframos  ese nuevo codigo a letra.
-         StrLet += Newlet;                         //Almacenamos letra por letra dentro del nuevo String.
+         strLet += Newlet;                         //Almacenamos letra por letra dentro del nuevo String.
       }                                            //en caso contrario si es MINUSCULA.
       else if(Asc>=97 && Asc<=122) {
         var NewAscm = (Asc-97 +33)%26 +97;         //en este caso la formula cambia porque el rango es otro.
         var NewLetm = String.fromCharCode(NewAscm);//Desciframos  ese nuevo codigo a letra.
-          StrLet += NewLetm;                      //Almacenamos letra por letra dentro del nuevo String.
+          strLet += NewLetm;                      //Almacenamos letra por letra dentro del nuevo String.
       }  
-  } return StrLet;                                  //Retornamos el nuevo string con las letras enncriptadas.
+  } return strLet;                                  //Retornamos el nuevo string con las letras enncriptadas.
 }
 
                                        //FUNCION PARA DESCIFRAR
 
 function decipher(phrase){
-  var StrLet ="";                                  //Creamos el string vacio donde se almacenara losnuevos valores.
+  var strLet ="";                                  //Creamos el string vacio donde se almacenara losnuevos valores.
   for(i=0;i<phrase.length;i++){                    //Recorremosla frase ingresada,Si esta en el rango de MAYUSCULA.
       var Asc = phrase.charCodeAt(i)               //Obtenemos el codigo ASCII de cada letra
       if(Asc>=65&& Asc<=90){                      //Rango para solo las mayusculas
          var NewAsc = (Asc+65 -33)%26 + 65;        // Aplicamos la formula para obtener la nueva position ASCII.
          var Newlet= String.fromCharCode(NewAsc);  //Desciframos  ese nuevo codigo a letra.
-         StrLet += Newlet;                         //Almacenamos letra por letra dentro del nuevo String.      
+         strLet += Newlet;                         //Almacenamos letra por letra dentro del nuevo String.      
       }                                            //en caso contrario si es MINUSCULA.
       else if(Asc >=97 && Asc <= 122){
         var NewAscm = (Asc+97 +33)%26 +97;         //en este caso la formula cambia porque el rango es otro.
         var NewLetm = String.fromCharCode(NewAscm);//Desciframos  ese nuevo codigo a letra.
-          StrLet += NewLetm;                      //Almacenamos letra por letra dentro del nuevo String.
+          strLet += NewLetm;                      //Almacenamos letra por letra dentro del nuevo String.
       }
-  }return StrLet;                                  //Retornamos el nuevo string con las letras enncriptadas.
+  }return strLet;                                  //Retornamos el nuevo string con las letras enncriptadas.
 }
 
 //Una vez validado todo pasamos a realizar la opcion que el usuario haya escogido y lo mostramos por medio de un alert()
