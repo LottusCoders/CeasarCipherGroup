3b5886d4f212a08625725e9e16dbcf5c298d4f7e
DDCenteno <danielacentenoramos@gmail.com>
2017-10-12 09:20:08 -0500

    identa

--- Javascript/app2.js
+++ Javascript/app2.js
@@ -16,16 +16,16 @@ function cipher(phrase){
   var strLet ="";                                  //Creamos el string vacio donde se almacenara losnuevos valores.
  
   for(i=0;i<phrase.length;i++){                    //Recorremosla frase ingresada,Si esta en el rango de MAYUSCULA.
-      var Asc = phrase.charCodeAt(i)               //Obtenemos el codigo ASCII de cada letra
-      if(Asc>=65 && Asc<=90){                      //Rango para solo las mayusculas
-         var NewAsc = (Asc-65 +33)%26 + 65;        // Aplicamos la formula para obtener la nueva position ASCII.
-         var Newlet= String.fromCharCode(NewAsc);  //Desciframos  ese nuevo codigo a letra.
-         strLet += Newlet;                         //Almacenamos letra por letra dentro del nuevo String.
+      var asc = phrase.charCodeAt(i)               //Obtenemos el codigo ASCII de cada letra
+      if(asc>=65 && asc<=90){                      //Rango para solo las mayusculas
+         var newAsc = (asc-65 +33)%26 + 65;        // Aplicamos la formula para obtener la nueva position ASCII.
+         var newlet= String.fromCharCode(newAsc);  //Desciframos  ese nuevo codigo a letra.
+         strLet += newlet;                         //Almacenamos letra por letra dentro del nuevo String.
       }                                            //en caso contrario si es MINUSCULA.
-      else if(Asc>=97 && Asc<=122) {
-        var NewAscm = (Asc-97 +33)%26 +97;         //en este caso la formula cambia porque el rango es otro.
-        var NewLetm = String.fromCharCode(NewAscm);//Desciframos  ese nuevo codigo a letra.
-          strLet += NewLetm;                      //Almacenamos letra por letra dentro del nuevo String.
+      else if(asc>=97 && asc<=122) {
+        var newAscm = (asc-97 +33)%26 +97;         //en este caso la formula cambia porque el rango es otro.
+        var newLetm = String.fromCharCode(newAscm);//Desciframos  ese nuevo codigo a letra.
+          strLet += newLetm;                      //Almacenamos letra por letra dentro del nuevo String.
       }  
   } return strLet;                                  //Retornamos el nuevo string con las letras enncriptadas.
 }
@@ -35,16 +35,16 @@ function cipher(phrase){
 function decipher(phrase){
   var strLet ="";                                  //Creamos el string vacio donde se almacenara losnuevos valores.
   for(i=0;i<phrase.length;i++){                    //Recorremosla frase ingresada,Si esta en el rango de MAYUSCULA.
-      var Asc = phrase.charCodeAt(i)               //Obtenemos el codigo ASCII de cada letra
-      if(Asc>=65&& Asc<=90){                      //Rango para solo las mayusculas
-         var NewAsc = (Asc+65 -33)%26 + 65;        // Aplicamos la formula para obtener la nueva position ASCII.
-         var Newlet= String.fromCharCode(NewAsc);  //Desciframos  ese nuevo codigo a letra.
-         strLet += Newlet;                         //Almacenamos letra por letra dentro del nuevo String.      
+      var asc = phrase.charCodeAt(i)               //Obtenemos el codigo ASCII de cada letra
+      if(asc>=65&& asc<=90){                      //Rango para solo las mayusculas
+         var newAsc = (asc+65 -33)%26 + 65;        // Aplicamos la formula para obtener la nueva position ASCII.
+         var newlet= String.fromCharCode(newAsc);  //Desciframos  ese nuevo codigo a letra.
+         strLet += newlet;                         //Almacenamos letra por letra dentro del nuevo String.      
       }                                            //en caso contrario si es MINUSCULA.
-      else if(Asc >=97 && Asc <= 122){
-        var NewAscm = (Asc+97 +33)%26 +97;         //en este caso la formula cambia porque el rango es otro.
-        var NewLetm = String.fromCharCode(NewAscm);//Desciframos  ese nuevo codigo a letra.
-          strLet += NewLetm;                      //Almacenamos letra por letra dentro del nuevo String.
+      else if(asc >=97 && asc <= 122){
+        var newAscm = (Asc+97 +33)%26 +97;         //en este caso la formula cambia porque el rango es otro.
+        var newLetm = String.fromCharCode(newAscm);//Desciframos  ese nuevo codigo a letra.
+          strLet += newLetm;                      //Almacenamos letra por letra dentro del nuevo String.
       }
   }return strLet;                                  //Retornamos el nuevo string con las letras enncriptadas.
 }
